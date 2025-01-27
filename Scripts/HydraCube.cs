71dbbcc8e875d188ce363e821d11c9c16481eb1b
H01RaSH1K <zfnkcy@gmail.com>
2025-01-27 12:54:42 +0400

    Resolving comments

--- Scripts/HydraCube.cs
+++ Scripts/HydraCube.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(Replicator))]
+[RequireComponent (typeof(Renderer))]
 [RequireComponent (typeof(Exploder))]
 public class HydraCube : MonoBehaviour
 {
+    [SerializeField] private Replicator _replicator;
+
     [SerializeField] private float _successChance = 1f;
     [SerializeField] private float _successChanceDivider = 2f;
+    [SerializeField] private float _scaleDivider = 2f;
 
-    private Replicator _replicator;
+    private Renderer _renderer;
     private Exploder _exploder;
 
-    private void Start()
+    public void OnReplicated(HydraCube parentCube)
     {
-        _replicator = GetComponent<Replicator>();
+        transform.localPosition = GetRandomOffsetPosition(parentCube);
+        transform.localScale = parentCube.transform.localScale / _scaleDivider;
+        _renderer.material.color = GetRandomColor();
+    }
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
         _exploder = GetComponent<Exploder>();
     }
 
@@ -26,10 +36,27 @@ public class HydraCube : MonoBehaviour
         if (_successChance > value)
         {
             _successChance /= _successChanceDivider;
-            var replicates = _replicator.Replicate();
+            var replicates = _replicator.Replicate(this);
             _exploder.Explode(replicates);
         }
 
         Destroy(gameObject);
+    }
+
+    private Vector3 GetRandomOffsetPosition(HydraCube parentCube)
+    {
+        var origin = parentCube.transform.position;
+        var maxOffset = parentCube.transform.localScale;
+
+        float offsetX = Random.Range(-maxOffset.x, maxOffset.x);
+        float offsetY = Random.Range(0, maxOffset.y);
+        float offsetZ = Random.Range(-maxOffset.z, maxOffset.z);
+
+        return new Vector3(origin.x + offsetX, origin.y + offsetY, origin.z + offsetZ);
+    }
+
+    private Color GetRandomColor()
+    {
+        return Random.ColorHSV();
     }
 }
--- Scripts/Replicator.cs
+++ Scripts/Replicator.cs
@@ -2,50 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[RequireComponent(typeof(Renderer))]
 public class Replicator : MonoBehaviour
 {
     [SerializeField] private int _minReplicates = 2;
     [SerializeField] private int _maxReplicates = 6;
 
-    [SerializeField] private float _scaleDivider = 2f;
-
-    public List<GameObject> Replicate()
+    public List<GameObject> Replicate(HydraCube cube)
     {
         var replicatesAmount = Random.Range(_minReplicates, _maxReplicates + 1);
         List<GameObject> replicates = new List<GameObject>();
 
         for (int i = 0; i < replicatesAmount; i++)
-            replicates.Add(InstatiateReplicate());
+        {
+            var replicate = Instantiate(cube);
+            replicate.OnReplicated(cube);
+            replicates.Add(replicate.gameObject);
+        }
 
         return replicates;
-    }
-
-    private GameObject InstatiateReplicate()
-    {
-        var replicate = Instantiate(gameObject);
-        replicate.transform.localPosition = GetRandomOffsetPosition();
-        replicate.transform.localScale = transform.localScale / _scaleDivider;
-        replicate.GetComponent<Renderer>().material.color = GetRandomColor();
-
-        return replicate;
-    }
-
-    private Vector3 GetRandomOffsetPosition()
-    {
-        var origin = transform.position;
-        var maxOffset = transform.localScale; 
-
-        float offsetX = Random.Range(-maxOffset.x, maxOffset.x);
-        float offsetY = Random.Range(0, maxOffset.y);
-        float offsetZ = Random.Range(-maxOffset.z, maxOffset.z);
-
-        return new Vector3(origin.x + offsetX, origin.y + offsetY, origin.z + offsetZ);
-    }
-
-    private Color GetRandomColor()
-    {
-        return Random.ColorHSV();
     }
 }
